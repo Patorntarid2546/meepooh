71d2e3afe3a2bbab1e2f96e794b5663ba2f52a93
Patorntarid2546 <chpooha@gmail.com>
2021-11-30 19:49:22 +0700

    lab 7 finish

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -3,39 +3,43 @@
 
 using namespace std;
 
-string func1(string x){
-	int i = 0, L = x.size();
-	string y = "";
+string Reversedtext(string m){
+	int i = 0, L = m.size();
+	string n = "";
 	while(i < L){
-		y += x[L-i-1];
+		n += m[L-i-1];
 		i++;
 	}
-	return y;
+	return n;
 }
 
-string func2(string x){
-	int i = 0, L = x.size();
-	string y = "";
+string func2(string m){
+	int i = 0, L = m.size();
+	string n = "";
 	while(i < L){
-		y += toupper(x[i]);
+		n += toupper(m[i]);
 		i++;
 	}
-	return y;	
+	return n;	
 }
 
-string func3(string x){
-	int i = 0, L = x.size();
-	string y = "";
-	while(i < L){
-		y += tolower(x[i]);
-		i++;
-	}
-	return y;	
-}
 
 int main(){
-    cout >> "Input text: "
-    cout >> "Reversed text: "
-    cout >> "Palindrome: "
+    
+    string a ;
+    string b ;
+    string n ;
+    cout << "Input text: ";
+    cin >> a ;
+    cout << "Reversed text: ";
+    b = Reversedtext(a);
+    cout << b <<"\n" ;
+    cout << "Palindrome: " ;
+    if (func2(a) == func2(b)){
+        cout << "Yes";
+    }
+    else{
+        cout << "No";
+    }
     return 0;
 }
