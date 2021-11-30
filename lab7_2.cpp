ca1959a67efa7d860dda0a2ae80117315739d0a7
Patorntarid2546 <chpooha@gmail.com>
2021-11-30 19:53:43 +0700

    lab 7_2 finish

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,28 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
+	int i = 0, M[5000], N;
 	while(i < 5000){
-		P[i] = 2*i+1;
+		M[i] = 2*i+1;
 		i++;
 	}
-	
+    cout << "Please input N: ";
+    cin >> N ;
+	if(N > 0){
+        int j=0,result=0;
+        while(j < 5000){
+            if(M[j]%N == 0){
+                result = result -M[j];
+            }
+            else{
+                result = result + N[j];
+            }
+            j++;
+        }
+        cout << "Result = " << result;
+    }	
+    else{
+		cout << "Invalid input!!!";
+	}
 	return 0;
 }
